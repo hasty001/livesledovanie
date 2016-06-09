91214e4cd559848dd77ca44fefb276255adb17cd
lcicon <ladislav.cicon@wandera.com>
2016-06-10 00:23:13 +0200

    a

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='sledovanie',
       author_email='info@cestasnp.sk',
       url='http://livesledovanie.eu/',
 
-      install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe', 'Pillow=3.2.0'],
+      install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe'],
       #install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe'],
      )
