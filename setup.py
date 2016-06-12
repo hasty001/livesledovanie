ee5fa18253eeefcc33237d90f7e2a38c55a20b95
lcicon <ladislav.cicon@wandera.com>
2016-06-12 11:21:19 +0200

    adding prillow

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='sledovanie',
-      version='1.0',
+      version='1.2',
       description='live sledovanie app',
       author='hasty',
       author_email='info@cestasnp.sk',
       url='http://livesledovanie.eu/',
 
-      install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe', 'Pillow=3.2.0'],
+      install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe', 'Pillow'],
       #install_requires=['flask==0.10.1','flask-login==0.2.7','sqlalchemy==0.9.8','flask-sqlalchemy==2.0', 'MarkupSafe'],
      )
