321272783a8ee531f2e07a50a557c13f81dbfa82
ladislav cicon <ladislav.cicon@wandera.com>
2016-08-06 20:19:27 +0200

    a

--- wsgi/sledovaniev2.py
+++ wsgi/sledovaniev2.py
@@ -324,7 +324,7 @@ def miesta():
         elif not request.form['lon']:
             flash('Text is required', 'error')
         else:
-            print("LOG %s" %datetime.datetime.now())
+            print("LOG %s" % datetime.now())
 
             miesto = {
                 'accuracy': int(float(request.form['accuracy'])),
@@ -333,7 +333,7 @@ def miesta():
                 'text': request.form['text'],
                 'img_url': filename,
                 'user_id': int(g.user.id),
-                'created': datetime.datetime.now()
+                'created': datetime.now()
             }
 
             miesto['coordinates'] = (float(request.form['lon']), float(request.form['lat']))
