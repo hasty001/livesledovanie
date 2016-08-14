22aeef4bb3dbd8bd805102ca54c148c9da75a3a8
ladislav cicon <ladislav.cicon@wandera.com>
2016-08-14 22:24:25 +0200

    zmena textu v doulezite miesta

--- wsgi/places.py
+++ wsgi/places.py
@@ -24,6 +24,14 @@ poi = db.poi
 
 @places.route('/ajax/pois', methods=['GET'])
 
+
+@places.route('/mapa', methods=['GET'])
+#@login_required
+def mapa():
+    if request.method == 'GET':
+        return render_template('mapa.html')
+
+
 # koli moznosti mat mapu verejne
 #@login_required
 def pois():
@@ -33,8 +41,30 @@ def pois():
         return pois
 
 
-@places.route('/mapa', methods=['GET'])
+
+@places.route('/ajax/pois/icons/<category>', methods=['GET'])
 #@login_required
-def mapa():
+def icons(category):
     if request.method == 'GET':
-        return render_template('mapa.html')
+        icons_to_png = {
+            'pramen': 'http://cestasnp.sk/web/images/mapa/watter.png',
+            'pristresok': 'http://cestasnp.sk/web/images/mapa/hut.png',
+            'utulna': 'http://cestasnp.sk/web/images/mapa/hut.png',
+            'chata': 'http://cestasnp.sk/web/images/mapa/cottage.png',
+            'potraviny': 'http://cestasnp.sk/web/images/mapa/grocery.png',
+            'krcma_jedlo': 'http://cestasnp.sk/web/images/mapa/nutrition.png'
+        }
+
+        icon = {
+            'iconUrl': icons_to_png[category],
+            #'iconRetinaUrl': 'my-icon@2x.png',
+            #'iconSize': [38, 95],
+            'iconAnchor': [22, 94],
+            'popupAnchor': [-3, -76],
+            #shadowUrl: 'my-icon-shadow.png',
+            #shadowRetinaUrl: 'my-icon-shadow@2x.png',
+            #shadowSize: [68, 95],
+            #shadowAnchor: [22, 94]
+        }
+
+        return jsonify(icon)
