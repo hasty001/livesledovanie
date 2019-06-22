c039b445053ada706b9fd8118ce4433c7873a42b
lcicon <ladislav.cicon@wandera.com>
2019-06-22 06:41:23 +0200

    a

--- sledovaniev2.py
+++ sledovaniev2.py
@@ -470,10 +470,9 @@ def miesta():
     return render_template('miesta.html')
 
 
-
+"""
 if __name__ == '__main__':
     app.run(debug = False)
 """
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-"""
